5d07eefb5228063039dfb1d995352a56bb963929
anugeeth <anugeeth2@gmail.com>
2020-01-11 18:34:13 +0530

    completed add todo

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -7,11 +7,24 @@
     </div>
 </template>
 <script>
+import uuid from 'uuid'
 export default {
     name:'AddTodo',
     data: () => ({
         title: ''
-    })
+    }),
+    methods: {
+        addTodo(e){
+            e.preventDefault()
+            const newTodo= {
+                id: uuid.v4(),
+                title : this.title,
+                completed: false
+            }
+            this.$emit('add-todo',newTodo)
+            this.title = ''
+        }
+    }
 }
 </script>
 <style scoped>
